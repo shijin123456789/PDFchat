c298e90ab91f157738a3cbacda7892fdaae39745
knightfury1307 <knightfury1307@gmail.com>
2023-08-17 19:56:01 +0530

    2nd commit added new functions

--- app.py
+++ app.py
@@ -1,50 +1,34 @@
-import streamlit as st
-#from dotenv import load_dotenv
-#from PyPDF2 import PdfReader
-#from langchain.text_splitter import CharacterTextSplitter
-#from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-#from langchain.vectorstores import FAISS
-#from langchain.chat_models import ChatOpenAI
-#from langchain.memory import ConversationBufferMemory
-#from langchain.chains import ConversationalRetrievalChain
-#from htmlTemplates import css, bot_template, user_template
-#from langchain.llms import HuggingFaceHub
+import streamlit as st 
+from dotenv import load_dotenv
+from processButton import get_pdf_text ,get_vectorstore ,get_text_chunks ,get_conversation_chain
 
 
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple files",page_icon=":books:")
+    
+    st.header("Chat with multiple PDF's")    
+    st.text_input("Ask a question about your document:")
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
 
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks[0])
+                
+                # create vector store
+                #vectorstore = get_vectorstore(text_chunks)
 
-st.set_page_config(page_title="Chat with multiple PDFs",
-                    page_icon=":books:")
-#    load_dotenv()
-
-#    st.write(css, unsafe_allow_html=True)
-
-# if "conversation" not in st.session_state:
-#     st.session_state.conversation = None
-# if "chat_history" not in st.session_state:
-#     st.session_state.chat_history = None
-st.header("Chat with multiple PDFs :books:")
-user_question = st.text_input("Ask a question about your documents:")
-#    if user_question:
-#        handle_userinput(user_question)
-
-with st.sidebar:
-    st.subheader("Your documents")
-    pdf_docs = st.file_uploader(
-        "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-#    if st.button("Process"):
-#        with st.spinner("Processing"):
-            # get pdf text
-#            raw_text = get_pdf_text(pdf_docs)
-
-            # get the text chunks
-#            text_chunks = get_text_chunks(raw_text)
-
-            # create vector store
-#            vectorstore = get_vectorstore(text_chunks)
-
-            # create conversation chain
-#            st.session_state.conversation = get_conversation_chain(
-#                vectorstore)
-
-
+                # create conversation chain
+                #st.session_state.conversation = get_conversation_chain(
+                 #   vectorstore)
+        
+        
+if __name__ == "__main__":
+    main()        
---
+++ processButton.py
@@ -0,0 +1,48 @@
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import HuggingFaceHub
+from langchain.chains import ConversationalRetrievalChain
+
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
